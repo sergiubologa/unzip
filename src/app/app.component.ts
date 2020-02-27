0647a5c70adbf45c256a095f01a2cf0cab479fc5
Sergiu Bologa <sergiu.bologa@uipath.com>
2020-02-27 18:03:40 +0200

    Create project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+
+import * as zip from 'jszip';
+import {
+    BehaviorSubject,
+    forkJoin,
+} from 'rxjs';
+
+interface IFile {
+  fileName: string;
+  content: ArrayBuffer;
+}
+
+interface IResult {
+  file: IFile;
+  compressedSize: string;
+  uncompressedSize: string;
+  memory: string;
+  time: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +26,76 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'unzip';
+  public files: IFile[] = [
+    { fileName: '1p-DOM', content: null },
+    { fileName: '2p-DOM', content: null },
+    { fileName: '11p-DOM', content: null },
+    { fileName: '110p-DOM', content: null },
+    { fileName: '223p-DOM', content: null },
+    { fileName: '446p-DOM', content: null },
+  ];
+  public results$ = new BehaviorSubject<IResult[]>([]);
+
+  constructor(
+    private _http: HttpClient,
+  ) {
+    const getRequests = this.files.map(c => this._get(c.fileName));
+
+    forkJoin(getRequests).subscribe((results) => {
+      results.forEach((content, index) => {
+        this.files[index].content = content;
+      });
+    });
+  }
+
+  public unzip(file: IFile) {
+    // @ts-ignore
+    const startMemory = performance.memory.usedJSHeapSize;
+    const t0 = performance.now();
+
+    zip.loadAsync(file.content)
+      .then(res => {
+        const zipFile = res.file(Object.entries(res.files)[0][0]);
+        zipFile.async('string').then(c => {
+          // @ts-ignore
+          const currentMemory = performance.memory.usedJSHeapSize;
+          const t1 = performance.now();
+
+          const result: IResult = {
+            // @ts-ignore
+            compressedSize: this._formatBytes(file._data.compressedSize),
+            // @ts-ignore
+            uncompressedSize: this._formatBytes(file._data.uncompressedSize),
+            file,
+            time: this._formatTime(t1 - t0),
+            memory: this._formatBytes(currentMemory - startMemory),
+          };
+          const newResults = this.results$.value;
+          newResults.push(result);
+          this.results$.next(newResults);
+        });
+      });
+  }
+
+  private _formatBytes(bytes, decimals = 2) {
+    if (bytes <= 0) { return '0 Bytes'; }
+
+    const k = 1024;
+    const dm = decimals < 0 ? 0 : decimals;
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+  }
+
+  private _formatTime(ms: number) {
+    return new Date(ms).toISOString().slice(11, -1);
+  }
+
+  private _get(file: string) {
+    return this._http.get(`assets/${file}.zip`, {
+      responseType: 'arraybuffer'
+    });
+  }
 }
